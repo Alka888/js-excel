3365951bbedd2ca0144da630c564166f7550ca2b
Alka888 <alicom19900215@gmail.com>
2021-02-21 19:23:47 -0600

    finish table resize

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -12,19 +12,16 @@ export class Excel {
     this.components = this.components.map((Component) => {
       const $el = $.create('div', Component.className)
       const component = new Component($el);
-      // DEBUG
-      if (component.name) {
-        window['c' + component.name] = component
-      }
+
       $el.html(component.toHTML())
       $root.append($el)
       return component
     });
     return $root;
   }
+
   render() {
     this.$el.append(this.getRoot());
-
     this.components.forEach(component => component.init())
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -14,6 +14,7 @@ class Dom {
     }
     return this.$el.outerHTML.trim()
   }
+
   clear() {
     this.html()
     return this
@@ -33,13 +34,33 @@ class Dom {
     if (node instanceof Dom) {
       node = node.$el
     }
-
     if (Element.prototype.append) {
       this.$el.append(node)
     } else {
       this.$el.appendChild(node)
     }
     return this
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => {
+      this.$el.style[key] = styles[key]
+    })
   }
 }
 
